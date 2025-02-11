2a5f39bd6c5c372756b52161b89fee8b63caf23f
damiankadziela81 <damiankadziela02@gmail.com>
2025-02-11 11:12:23 +0100

    enabled headless with slow mode

--- src/test/java/org/example/firstTest.java
+++ src/test/java/org/example/firstTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import org.assertj.core.api.Assertions;
@@ -11,10 +12,10 @@ class firstTest {
     @Test
     void test1() {
         Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
         Page page = browser.newPage();
 
         page.navigate("https://playwright.dev");
-        Assertions.assertThat(page.title()).isEqualTo("Playwright");
+        Assertions.assertThat(page.title()).isEqualTo("Fast and reliable end-to-end testing for modern web apps | Playwright");
     }
 }
