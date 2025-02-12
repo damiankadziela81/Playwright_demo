0599a0206d96cd60eaca793b4ecb2ad3530c41d0
damiankadziela81 <damiankadziela02@gmail.com>
2025-02-12 10:06:13 +0100

    added more simple tests

--- src/test/java/org/example/module_4/LoginWithValidCredentials.java
+++ src/test/java/org/example/module_4/LoginWithValidCredentials.java
@@ -13,6 +13,7 @@ class LoginWithValidCredentials extends BaseTest {
     @Test
     void shouldLoginWithValidCredentials() {
         page.navigate("https://the-internet.herokuapp.com/login");
+//        page.pause(); // can be used to record user actions and generate code!
         page.getByLabel("Username").fill("tomsmith");
         page.getByLabel("Password").fill("SuperSecretPassword!");
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
@@ -20,4 +21,32 @@ class LoginWithValidCredentials extends BaseTest {
         page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Logout")).click();
         PlaywrightAssertions.assertThat(page.getByText("You logged out of the secure area!")).isVisible();
     }
+
+    @Test
+    void shouldNotLoginWithoutCredentials() {
+        page.navigate("https://the-internet.herokuapp.com");
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Form Authentication")).click();
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
+        PlaywrightAssertions.assertThat(page.getByText("Your username is invalid!")).isVisible();
+    }
+
+    @Test
+    void shouldNotLoginWithInvalidUsername() {
+        page.navigate("https://the-internet.herokuapp.com");
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Form Authentication")).click();
+        page.getByLabel("Username").fill("tom");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
+        PlaywrightAssertions.assertThat(page.getByText("Your username is invalid!")).isVisible();
+    }
+
+    @Test
+    void shouldNotLoginWithInvalidPassword() {
+        page.navigate("https://the-internet.herokuapp.com");
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Form Authentication")).click();
+        page.getByLabel("Username").fill("tomsmith");
+        page.getByLabel("Password").fill("password");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
+        PlaywrightAssertions.assertThat(page.getByText("Your password is invalid!")).isVisible();
+    }
+
 }
